cf9db9459d3d23ecd7b519f2c3c82a20885ec7b1
Lucas sobczak <lucassobczak@Lucass-MacBook-Pro.local>
2024-10-23 21:28:04 -0400

    updates again

--- lua/sobczakl/lazy/colors.lua
+++ lua/sobczakl/lazy/colors.lua
@@ -1,23 +1,21 @@
 function ColorMyPencils(color)
-	color = "rose-pine"
-	vim.cmd.colorscheme(color)
+    color = "rose-pine"
+    vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
     {
-        "rose-pine/neovim",
-        name = "rose-pine",
+        'sainnhe/sonokai',
+        lazy = false,
+        priority = 1000,
         config = function()
-            require('rose-pine').setup({
-                disable_background = true,
-                styles = {
-                    italic = false,
-                },
-            })
-            ColorMyPencils()
+            -- Optionally configure and load the colorscheme
+            -- directly inside the plugin declaration.
+            vim.g.sonokai_enable_italic = true
+            vim.cmd.colorscheme('sonokai')
         end
     },
 
