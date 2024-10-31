88c7508cf7b5c973202fc0b801027557333db1ab
Lucas sobczak <lucassobczak3@gmail.com>
2024-10-31 10:09:47 -0400

    update to config

--- lua/sobczakl/lazy/toggleterm.lua
+++ lua/sobczakl/lazy/toggleterm.lua
@@ -10,7 +10,7 @@ return {
         toggleterm.setup {
             size = 20,
             open_mapping = [[<c-t>]],
-            hide_numbers = true,
+           hide_numbers = true,
             shade_filetypes = {},
             shade_terminals = true,
             shading_factor = 2,
@@ -28,7 +28,6 @@ return {
                     background = "Normal",
                 },
             },
-            -- Removed winbar section to prevent related errors
         }
 
         -- Set terminal key mappings
